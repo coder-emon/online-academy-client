98dbed7cadbbc7f0fe66ac4bf0838c8a73c81ea1
coder-emon <developeremon.me@gmail.com>
2023-01-17 18:25:10 +0600

    Nav Component done

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,10 +1,141 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import { FaBars, FaMoon, FaSun } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
 const Nav = () => {
+  const user = {
+    img: logo,
+    uid: "123",
+  };
+
+  const [displayNav, setDisplayNav] = useState(false);
+  //Theme Switcher start
+  const [isDark, setIsDark] = useState();
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    setIsDark(true);
+    document.documentElement.classList.add("dark");
+  };
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+
+    setIsDark(false);
+    document.documentElement.classList.remove("dark");
+  };
+  useEffect(() => {
+    const isTheme = localStorage.theme;
+    if (isTheme) {
+      setIsDark(true);
+    } else {
+      setIsDark(false);
+    }
+  }, []);
+  //Theme Switcher end
   return (
-    <div>
-      <h2>nav</h2>
+    <div className="bg-indigo-600 dark:shadow-lg dark:backdrop-blur-md dark:bg-gray-800/90 relative px-6 py-2">
+      <nav className="flex justify-between items-center relative  ">
+        <Link className="bg-white  rounded-lg active ">
+          <img src={logo} alt="" className="w-[80px]" />
+        </Link>
+        <ul className="md:flex hidden text-white md:space-x-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="faq">Faq</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          {user?.uid ? (
+            <img
+              src={user.img}
+              alt={user.diplayName}
+              className="h-8 w-8 rounded-full"
+            />
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
+          <button className="text-2xl ">
+            {isDark ? (
+              <FaSun onClick={setLight}></FaSun>
+            ) : (
+              <FaMoon onClick={setDark}></FaMoon>
+            )}
+          </button>
+        </ul>
+        <div className="flex md:hidden justify-between items-center gap-2 text-white text-2xl">
+          <button>
+            {isDark ? (
+              <FaSun onClick={setLight}></FaSun>
+            ) : (
+              <FaMoon onClick={setDark}></FaMoon>
+            )}
+          </button>
+          <button
+            onClick={() => {
+              setDisplayNav(!displayNav);
+            }}
+          >
+            {displayNav ? <ImCross></ImCross> : <FaBars></FaBars>}
+          </button>
+        </div>
+        {displayNav ? (
+          <ul className=" md:hidden flex flex-col top-14 justify-center items-center py-3 gap-2 bg-slate-500 absolute w-full  right-0  text-white md:space-x-4">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="faq">Faq</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            {user?.uid ? (
+              <>
+                <img
+                  src={user.img}
+                  alt={user.diplayName}
+                  className="h-8 w-8 rounded-full"
+                />
+                <button className="bg-blue-500 dark:bg-sky-800 dark:hover:bg-sky-900 hover:bg-blue-600 py-2 px-3 rounded">
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        ) : null}
+      </nav>
+      <div className=""></div>
     </div>
   );
 };
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../Components/Blog/Blog";
+import BlogDetails from "../Components/BlogDetails/BlogDetails";
+import Contact from "../Components/Contact/Contact";
 import CourseCard from "../Components/CourseCard/CourseCard";
 import Courses from "../Components/Courses/Courses";
+import Faq from "../Components/Faq/Faq";
 import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
 import Main from "../Main/Main";
 
 export const router = createBrowserRouter([
@@ -25,6 +31,30 @@ export const router = createBrowserRouter([
         path: "/course:id",
 
         element: <CourseCard></CourseCard>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/blog/:id",
+        element: <BlogDetails></BlogDetails>,
+      },
+      {
+        path: "/faq",
+        element: <Faq></Faq>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
